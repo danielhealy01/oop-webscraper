2d0eedd9114129577648626506543f08932848fc
DanielHealy <danielhealyhosting3@gmail.com>
2023-09-18 07:37:01 +0100

    Initial commit. Skeleton of functions.

---
+++ index.js
@@ -0,0 +1,3 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
---
+++ webScraper.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+class webScrapper {
+    constructor() {
+        this.url = ''
+        this.outputFile = ''
+    }
+
+    async initialise() {
+
+    }
+
+    async scrape() {
+        return data
+    }
+
+    async run() {
+        await this.initialise()
+        await this.scrape()
+    }
+}
+
+// TDD
+// Commander.js with argv
