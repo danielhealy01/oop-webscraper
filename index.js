dc1fd9de582ddf0fd3aa0dfb92cbffc06ec8509c
DanielHealy <danielhealyhosting3@gmail.com>
2023-09-18 16:02:03 +0100

    more sketoning of initial design

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const { WebScraper } = require('./webScraper')
+
+const scraper = new WebScraper('a', 'b')
+
+scraper.run()
 
--- webScraper.js
+++ webScraper.js
@@ -1,25 +1,37 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
 
-class webScrapper {
-    constructor() {
-        this.url = ''
-        this.outputFile = ''
-    }
+class WebScraper {
+	constructor() {
+		this.url = '';
+		this.outputFile = '';
+	}
 
-    async initialise() {
+	async initialise() {
+		await new Promise((resolve, reject) => {
+			setTimeout(() => resolve(), 2000);
+		});
+		console.log('resolved');
+	}
 
-    }
+	async scrape() {
+		try {
+			await new Promise((resolve, reject) => {
+				setTimeout(() => resolve(), 2000);
+			});
+			console.log('resolved');
+		} catch (err) {
+			console.log(`Error: ${err}`);
+		}
+	}
 
-    async scrape() {
-        return data
-    }
-
-    async run() {
-        await this.initialise()
-        await this.scrape()
-    }
+	async run() {
+		await this.initialise();
+		await this.scrape();
+	}
 }
+
+module.exports = { WebScraper };
 
 // TDD
 // Commander.js with argv
